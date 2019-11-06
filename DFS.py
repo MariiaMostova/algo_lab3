ca97a19c19ae382cc512be7af79f42154d2427f5
Mariia Mostova <were3437@gmail.com>
2019-11-06 12:03:00 +0200

    Edited code

--- DFS.py
+++ DFS.py
@@ -2,36 +2,35 @@ from collections import defaultdict
 
 
 class Graph:
-    def __init__(self, v_count):
+    def __init__(self, count):
         self.graph = defaultdict(list)
-        self.v_count = v_count
+        self.count = count
 
     def __del__(self):
         pass
 
-    def add_edge(self, u, v_count):
-        self.graph[u].append(v_count)
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
 
-    def recursive_dnf(self, v_count, visited_v, v_stack, cycle):
-        visited_v[v_count] = True
-        v_stack[v_count] = True
-        for edge_v in self.graph[v_count]:
-            if not visited_v[edge_v]:
-                if self.recursive_dnf(edge_v, visited_v, v_stack, cycle):
-                    cycle.append(edge_v)
-                    return True
+    def recursive_dfs(self, v, visited_v, v_stack, cycle):
+        visited_v[v] = True
+        v_stack[v] = True
+        for edge_v in self.graph[v]:
+            if not visited_v[edge_v] and self.recursive_dfs(edge_v, visited_v, v_stack, cycle):
+                cycle.append(edge_v)
+                return True
             elif v_stack[edge_v]:
                 cycle.append(edge_v)
                 return True
-        v_stack[v_count] = False
+        v_stack[v] = False
         return False
 
     def detect_cycle(self):
-        visited_v = [False for i in range(self.v_count)]
-        v_stack = [False for i in range(self.v_count)]
+        visited_v = [False for i in range(self.count)]
+        v_stack = [False for i in range(self.count)]
         cycle = []
-        for node in range(self.v_count):
+        for node in range(self.count):
             if not visited_v[node]:
-                if self.recursive_dnf(node, visited_v, v_stack, cycle):
+                if self.recursive_dfs(node, visited_v, v_stack, cycle):
                     return cycle
         return False
